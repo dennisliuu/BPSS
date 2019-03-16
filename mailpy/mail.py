65c00f9be1f7553bf7328385e2e1bbc799db5004
dennisliuu <dennisliuu@gmail.com>
2019-03-16 08:24:27 +0800

    update mail.py

--- mailpy/mail.py
+++ mailpy/mail.py
@@ -4,25 +4,19 @@ gmail_user = 'XXXX@gmail.com'
 gmail_password = 'XXXX'
 
 fromur = gmail_user  
-to = ['ayasedd@yahoo.com.tw']  
-subject = 'YUIGAHAMA IS MY WIFE'  
-body = 'YUIGAHAMA IS MY WIFEYUIGAHAMA IS MY WIFEYUIGAHAMA IS MY WIFEYUIGAHAMA IS MY WIFE'
+to = ['ayasedd@yahoo.com.tw']
 
-email_text = """\  
-From: %s  
-To: %s  
-Subject: %s
-
-%s
-""" % (fromur, ", ".join(to), subject, body)
+msg = MIMEText('content')
+msg['Subject'] = 'Test'
+msg['From'] = gmail_user
+msg['To'] = 'xxx@gmail.com'
 
 try:  
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
     server.login(gmail_user, gmail_password)
-    while 1:
-    	server.sendmail(fromur, to, email_text)
-    server.close()
+    server.send_message(msg)
+    server.quit()
     print('Email sent')
 except:  
     print('GG')
