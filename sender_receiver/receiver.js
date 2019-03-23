1ffb7652c2e47f28bc446e3e773899279b53f9b0
dennisliuu <dennisliuu@gmail.com>
2019-03-23 16:02:26 +0800

    merge nc-test

--- sender_receiver/receiver.js
+++ sender_receiver/receiver.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const NetcatClient = require('netcat/client')
 const nc = new NetcatClient()
-nc.addr('140.124.72.190').port(2389).connect().pipe(fs.createWriteStream('buf/send.txt'))
 
-let buff = fs.readFileSync('buf/send.txt', 'utf8')
-let	buff2 = new Buffer.from(buff, 'base64')
-fs.writeFileSync('2.pdf', buff2, err => {
-    if (err) throw err
-	console.log('Save !')
-})
+nc.addr('140.124.72.190').port(2389).connect().pipe(
+    fs.createWriteStream('buf/send.txt'),
+    setTimeout(() => {
+        let buf = fs.readFileSync('buf/send.txt', 'utf8')
+        let buff = new Buffer.from(buf, 'base64')
+        fs.writeFileSync('2.pdf', buff, err => {
+            if (err) throw err
+            console.log('Save !')
+        })
+    }, 2000)
+)
