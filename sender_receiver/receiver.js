2ae8ee1c53698bdac4d0687728be9558b0326851
dennisliuu <dennisliuu@gmail.com>
2019-03-25 02:21:23 +0800

    add encrypt and decrypt

--- sender_receiver/receiver.js
+++ sender_receiver/receiver.js
@@ -1,6 +1,13 @@
 const fs = require('fs')
+const path = require('path')
+
 const NetcatClient = require('netcat/client')
 const nc = new NetcatClient()
+
+const NodeRSA = require('node-rsa')
+const generator = require('../BCPtest/utils/generator')
+const encryptFile = require('../BCPtest/utils/encryptFile')
+const decryptFile = require('../BCPtest/utils/decryptFile')
 
 const rec = () => {
     nc.addr('140.124.72.190').port(2389).connect().pipe(
@@ -8,21 +15,39 @@ const rec = () => {
     )
 }
 
-const buf2pdf = () => {
-    let buf = fs.readFileSync('buf/send.txt', 'utf8')
+const buf2pdf = (paperTXT) => {
+    let buf = paperTXT
     let buff = new Buffer.from(buf, 'base64')
-    fs.writeFileSync('2.pdf', buff, err => {
+    fs.writeFileSync('store/2.pdf', buff, err => {
         if (err) throw err
         console.log('Save !')
     })
+}
+
+const readTXT = () => {
+    return new Promise(resolve => {
+		fs.readFile('buf/send.txt', (err, data) => {
+			if (err) console.log(err)
+			resolve(data)
+		})
+	})
 }
 
 const main = async () => {
     if (process.argv[2] == 'r') {
         await rec()
     }
-    else if (process.argv[2] == 'e') {
-        await buf2pdf()
+    else if (process.argv[2] == 'd') {
+        let paperTXT = await readTXT()
+        let key = path.join('pem', 'private.pem')
+        console.log(key)
+        paperTXT = await decryptFile(paperTXT, key)
+        // for (let i = 3; i < process.argv.length; i++) {
+        //     const e = process.argv[i]
+        //     let paperTXT = await decryptFile(paperTXT, e)    
+        // }
+        // console.log(paperTXT)
+        // await buf2pdf(paperTXT)
     }
     else {
         console.log("NOT CORRECT ARGV")
--- sender_receiver/sender.js
+++ sender_receiver/sender.js
@@ -1,11 +1,41 @@
 const fs = require('fs')
-let buffer = fs.readFileSync('./store/send.pdf')
-let bs64 = buffer.toString('base64')
-fs.writeFile('buf/send.txt', bs64, err => {
-	if (err) throw err
-	console.log('Send !')
-})
+const NodeRSA = require('node-rsa')
+const encryptFile = require('../BCPtest/utils/encryptFile')
 
-const NetcatServer = require('netcat/server')
-const nc = new NetcatServer()
-nc.port(2389).serve('buf/send.txt').listen()
+const pdf2bs64 = () => {
+	let buffer = fs.readFileSync('./store/paper.pdf')
+	let bs64 = buffer.toString('base64')
+	fs.writeFile('buf/send.txt', bs64, err => {
+		if (err) throw err
+		console.log('pdf complete!')
+	})
+}
+
+const readTXT = () => {
+    return new Promise(resolve => {
+		fs.readFile('buf/send.txt', (err, data) => {
+			if (err) console.log(err)
+			resolve(data)
+		})
+	})
+}
+
+const sender = (eA) => {
+	fs.writeFile('buf/send.txt', eA, err => {
+		if (err) throw err
+		console.log('Send !')
+	})
+	const NetcatServer = require('netcat/server')
+	const nc = new NetcatServer()
+	nc.port(2389).serve('buf/send.txt').listen()
+}
+
+const main = async () => {
+	await pdf2bs64()
+	let paperTXT = await readTXT()
+	// console.log('文本 (N)：' + paperTXT)
+	let eA = await encryptFile(paperTXT, '../BCPtest/pem/privateA.pem')
+	sender(eA)
+}
+
+main()
