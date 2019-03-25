6e5131d7c0d4397c0f110599122b6f0c9615bcf1
dennisliuu <dennisliuu@gmail.com>
2019-03-25 09:27:49 +0800

    add generator.js

---
+++ sender_receiver/generator.js
@@ -0,0 +1,5 @@
+const fs = require('fs')
+
+const generator = require("../BCPtest/utils/generator")
+
+generator('private', 'public')
--- sender_receiver/receiver.js
+++ sender_receiver/receiver.js
@@ -1,57 +1,52 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-const NetcatClient = require('netcat/client')
-const nc = new NetcatClient()
+const NetcatClient = require("netcat/client");
+const nc = new NetcatClient();
 
-const NodeRSA = require('node-rsa')
-const generator = require('../BCPtest/utils/generator')
-const encryptFile = require('../BCPtest/utils/encryptFile')
-const decryptFile = require('../BCPtest/utils/decryptFile')
+const NodeRSA = require("node-rsa");
+const decryptFile = require("../BCPtest/utils/decryptFile");
 
 const rec = () => {
-    nc.addr('140.124.72.190').port(2389).connect().pipe(
-        fs.createWriteStream('buf/send.txt')
-    )
+  nc.addr("140.124.72.190")
+    .port(2389)
+    .connect()
+    .pipe(fs.createWriteStream("buf/send.txt"))
 }
 
-const buf2pdf = (paperTXT) => {
-    let buf = paperTXT
-    let buff = new Buffer.from(buf, 'base64')
-    fs.writeFileSync('store/2.pdf', buff, err => {
-        if (err) throw err
-        console.log('Save !')
-    })
+const buf2pdf = paperTXT => {
+  let buf = paperTXT
+  let buff = new Buffer.from(buf, "base64")
+  fs.writeFileSync("store/2.pdf", buff, err => {
+    if (err) throw err
+    console.log("Save !")
+  })
 }
 
 const readTXT = () => {
-    return new Promise(resolve => {
-		fs.readFile('buf/send.txt', 'utf8', (err, data) => {
-			if (err) console.log(err)
-			resolve(data)
-		})
-	})
+  return new Promise(resolve => {
+    fs.readFile("buf/send.txt", "utf8", (err, data) => {
+      if (err) console.log(err)
+      resolve(data)
+    })
+  })
 }
 
 const main = async () => {
-    if (process.argv[2] == 'r') {
-        await rec()
-    }
-    else if (process.argv[2] == 'd') {
-        let paperTXT = await readTXT()
-        let key = path.join('pem', 'private.pem')
-        
-        paperTXT = await decryptFile(paperTXT, '../BCPtest/pem/publicAB.pem')
-        // for (let i = 3; i < process.argv.length; i++) {
-        //     const e = process.argv[i]
-        //     let paperTXT = await decryptFile(paperTXT, e)    
-        // }
-        console.log(paperTXT)
-        // await buf2pdf(paperTXT)
-    }
-    else {
-        console.log("NOT Correct argv")
-    }
+  switch (process.argv[2]) {
+    case "r":
+      await rec();
+      break
+    case "d":
+      let paperTXT = await readTXT()
+      let key = path.join("pem", "public.pem")
+      paperTXT = await decryptFile(paperTXT, key)
+      console.log(paperTXT);
+      await buf2pdf(paperTXT)
+      break
+    default:
+      console.log("No argv call " + process.argv[2] + ".")
+  }
 }
 
-main()
+main();
