59073d3ca8b6a2cd552bd4fc0ea6cf80a6687571
dennisliuu <dennisliuu@gmail.com>
2019-03-30 17:28:21 +0800

    update backend

--- koa-body-demo/routes/index.js
+++ koa-body-demo/routes/index.js
@@ -2,19 +2,26 @@ const router = require("koa-router")();
 const koaBody = require("koa-body");
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://140.124.72.124:8080/paper", {
-//   useNewUrlParser: true
-// });
-// const Block = mongoose.model("Block", {
-//   fName: String,
-//   lName: String,
-//   tel: String,
-//   org: String,
-//   email: String,
-//   abstract: String,
-//   orcid: String,
-//   paper_txt: String
-// });
+mongoose.connect("mongodb://140.124.72.124:8080/paper", {
+  useNewUrlParser: true
+});
+
+const Block = mongoose.model("Block", {
+  fName: String,
+  lName: String,
+  tel: String,
+  org: String,
+  email: String,
+  abstract: String,
+  orcid: String,
+  paper_txt: String
+});
+
+const Blockchain = mongoose.model("Blockchain", {
+	_id: String,
+	"Block Number": String,
+	Hash: String
+}, 'blockchain')
 
 router.get("/", async ctx => {
   await ctx.render("index");
@@ -26,24 +33,15 @@ router.get("/", async ctx => {
 //   ctx.body = JSON.stringify(ctx.request.file);
 // });
 
-// router.post("/", async ctx => {
-//   ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
-//   // console.log(JSON.stringify(ctx.request.body));
-//   const new_block = new Block(ctx.request.body);
-//   console.log(new_block);
-//   // console.log(kitty2)
-//   new_block.save().then(() => console.log("Add success!"));
-//   // kitty2.save().then(() => console.log('meow2'))
-// });
-
-mongoose.connect("mongodb://140.124.72.124:8080/MongoDB_Plus_Blockchain", {
-  useNewUrlParser: true
+router.post("/", async ctx => {
+  ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+  // console.log(JSON.stringify(ctx.request.body));
+  const new_block = new Block(ctx.request.body);
+  console.log(new_block);
+  // console.log(kitty2)
+  new_block.save().then(() => console.log("Add success!"));
+  // kitty2.save().then(() => console.log('meow2'))
 });
-const Blockchain = mongoose.model("Block", {
-  _id: String,
-  "Block Number": String,
-  Hash: String
-})
 
 router.get("/blocks", async ctx => {
 
--- mongoose_test/test.js
+++ mongoose_test/test.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://140.124.72.124:8080/MongoDB_Plus_Blockchain", {
+mongoose.connect("mongodb://140.124.72.124:8080/paper", {
   useNewUrlParser: true
 });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
-/*const Block = mongoose.model("Block", {
+const Block = mongoose.model("Block", {
   fName: String,
   lName: String,
   tel: String,
@@ -14,26 +14,33 @@ db.on("error", console.error.bind(console, "connection error:"));
   abstract: String,
   orcid: String
 });
-*/
 
-const Block = mongoose.model("Block", {
+const Blockchain = mongoose.model("Blockchain", {
 	_id: String,
 	"Block Number": String,
 	Hash: String
-})
+}, 'blockchain')
 
 // const kitty = new Cat({ name: "jimmy" });
 // kitty.save((err, kitty) => {
 //   console.log(err, kitty);
 // });
 
-Block.find({}, function(err, blocks) {
+// Block.find({}, function(err, blocks) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(blocks);
+//   }
+// });
+
+Blockchain.find({}, function(err, blocks) {
   if (err) {
     console.log(err);
   } else {
     console.log(blocks);
   }
 });
-// Block.deleteOne({ fName: 'dennis' }, function (err) {
-// 	if (err) throw err;
-// });
+Block.deleteOne({ fName: 'dennis' }, function (err) {
+	if (err) throw err;
+});
