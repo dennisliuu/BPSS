1adbed807a171d7c4f81f2991ab0b34ef2c85e88
dennisliuu <dennisliuu@gmail.com>
2019-04-01 22:56:24 +0800

    update koa router

--- koa-body-demo/routes/index.js
+++ koa-body-demo/routes/index.js
@@ -1,27 +1,5 @@
 const router = require("koa-router")();
-const koaBody = require("koa-body");
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://140.124.72.124:8080/paper", {
-  useNewUrlParser: true
-});
-
-const Block = mongoose.model("Block", {
-  fName: String,
-  lName: String,
-  tel: String,
-  org: String,
-  email: String,
-  abstract: String,
-  orcid: String,
-  paper_txt: String
-});
-
-const Blockchain = mongoose.model("Blockchain", {
-	_id: String,
-	"Block Number": String,
-	Hash: String
-}, 'blockchain')
+const getData = require('../utils/getData')
 
 router.get("/", async ctx => {
   await ctx.render("index");
@@ -38,31 +16,15 @@ router.post("/", async ctx => {
   // console.log(JSON.stringify(ctx.request.body));
   const new_block = new Block(ctx.request.body);
   console.log(new_block);
-  // console.log(kitty2)
   new_block.save().then(() => console.log("Add success!"));
-  // kitty2.save().then(() => console.log('meow2'))
 });
 
 router.get("/blockchains", async ctx => {
-  await Blockchain.find({}, (err, blocks) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(blocks);
-      ctx.body = blocks
-    }
-  });
+  ctx.body = await getData.getBlockchain()
 })
 
 router.get("/blocks", async ctx => {
-  await Block.find({}, (err, blocks) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(blocks);
-      ctx.body = blocks
-    }
-  });
+  ctx.body = await getData.getBlock()
 })
 
 module.exports = router;
