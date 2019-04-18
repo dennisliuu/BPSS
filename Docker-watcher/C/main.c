47a44a88ee58f6a39f72178a25d192cb23108ffe
dennisliuu <dennisliuu@gmail.com>
2019-04-18 22:06:10 +0800

    add commend

--- Docker-watcher/C/main.c
+++ Docker-watcher/C/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main() {
-	FILE *ls = popen("sudo docker ps | grep PORTS", "r");
-	char buf[256];
-	if (fgets(buf, sizeof(buf), ls)) {
-		printf("%s", buf);
+int main()
+{
+	while (1)
+	{
+		// Replace TARGET_IMAGE to images name
+		FILE *ls = popen("sudo docker ps | grep TARGET_IMAGE", "r");
+		char buf[256];
+		if (fgets(buf, sizeof(buf), ls))
+		{
+			printf("%s", buf);
+		}
+		else
+			system("echo Not running");
+		pclose(ls);
+		// Every 10 mins check.
+		sleep(3600);
 	}
-	else
-		system("echo Not running");
-	pclose(ls);
 }
