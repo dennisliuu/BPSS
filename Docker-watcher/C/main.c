7162ebc224b4552c2a0d3869bc9d8cb81faf6854
dennisliuu <dennisliuu@gmail.com>
2019-04-23 03:49:33 +0800

    add argv

--- Docker-watcher/C/main.c
+++ Docker-watcher/C/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	while (1)
 	{
@@ -11,16 +11,16 @@ int main()
 		char buf[256];
 		fgets(buf, sizeof(buf), ls);
 		// Replace TARGET_IMAGE to images name
-		char* pPosition = strstr(buf, "TARGET_IMAGE");
+		char* pPosition = strstr(buf, argv[1]);
 		if (pPosition)
 		{
-			printf("%s running", pPosition);
+			printf("%s running\n", pPosition);
 		}
 		else
 			system("echo Not running");
 			// DO SOMETHING
 		pclose(ls);
 		// Every 10 mins check.
-		sleep(600);
+		sleep(1);
 	}
 }
