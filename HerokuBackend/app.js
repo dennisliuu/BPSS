5002da0c310af3b1484223cca5453f1d3f523a17
dennisliuu <dennisliuu@gmail.com>
2019-07-23 18:28:05 +0800

    add backend

---
+++ HerokuBackend/app.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const koa = require('koa');
+const nunjucks = require('koa-nunjucks-2');
+const koaBody = require('koa-body');
+const router = require('./routes');
+
+const getUploadFileExt = require('./utils/getUploadFileExt');
+const getUploadFileName = require('./utils/getUploadFileName');
+const checkDirExist = require('./utils/checkDirExist');
+const getUploadDirName = require('./utils/getUploadDirName');
+const hashFile = require('./utils/hashFile')
+const postMongoDB = require('./utils/postMongoDB')
+
+const cors = require('@koa/cors');
+
+const app = new koa();
+app.use(cors());
+
+app.use(nunjucks({
+  ext: 'html',
+  path: path.join(__dirname, './views'),
+  nunjucksConfig: {
+    trimBlocks: true
+  }
+}));
+
+app.use(koaBody({
+  multipart: true,
+  // encoding: 'utf-8',
+  // formidable: {
+  //   uploadDir: path.join(__dirname, 'public/upload'),
+  //   keepExtensions: true,
+  //   // maxFieldsSize: 2 * 1024 * 1024,
+  //   onFileBegin: (name, file) => {
+  //     console.log(file);
+  //     const ext = getUploadFileExt(file.name);
+  //     const dirName = getUploadDirName();
+  //     const dir = path.join(__dirname, `public/upload/${dirName}`);
+  //     checkDirExist(dir);
+  //     const fileName = getUploadFileName(ext);
+  //     // const fileName = file.name;
+  //     file.path = `${dir}/${fileName}`;
+  //     app.context.uploadpath = app.context.uploadpath ? app.context.uploadpath : {};
+  //     app.context.uploadpath[name] = `${dirName}/${fileName}`;
+  //     // hashFile(dirName, fileName)
+  //     // postMongoDB(file.name)
+  //   },
+  // }
+}));
+app.use(router.routes()).use(router.allowedMethods());
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('[ok] Server starts at http://127.0.0.1:3000');
+});
+
---
+++ HerokuBackend/routes/index.js
@@ -0,0 +1,72 @@
+const Koa = require('koa')
+const router = require('koa-router')()
+// const getData = require('../utils/getData')
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://140.124.72.124:8080/paper", {
+    useNewUrlParser: true
+});
+
+const Block = mongoose.model("Block", {
+    fullName: String,
+    tel: String,
+    org: String,
+    email: String,
+    title: String,
+    abstract: String,
+    orcid: String,
+    paper_txt: String,
+    date: String,
+    time: String
+});
+const getBlock = () =>
+    new Promise((resolve, reject) => {
+        Block.find({}, (err, blocks) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(blocks);
+                resolve(blocks)
+            }
+        })
+    })
+
+const Blockchain = mongoose.model("Blockchain", {
+    _id: String,
+    "Block Number": String,
+    Hash: String
+}, 'blockchain')
+const getBlockchain = () =>
+    new Promise((resolve, reject) => {
+        Blockchain.find({}, (err, blocks) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(blocks);
+                resolve(blocks)
+            }
+        })
+    })
+
+router.get("/", async ctx => {
+  await ctx.render("index");
+});
+
+router.post("/", ctx => {
+  ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+  console.log(JSON.stringify(ctx.request.body));
+  const new_block = new Block(ctx.request.body);
+  console.log(new_block);
+  new_block.save().then(() => console.log("Add success!"));
+});
+
+router.get("/blockchains", async ctx => {
+  ctx.body = JSON.stringify(await getBlockchain(), null, "\t")
+})
+
+router.get("/blocks", async ctx => {
+  ctx.body = JSON.stringify(await getBlock(), null, "\t")
+})
+
+module.exports = router;
